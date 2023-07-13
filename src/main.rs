7bc13c926093073b481196e280f6a84884d21261
Christoffer Gram <christofferkgram@gmail.com>
2023-07-13 22:07:24 +0200

    Display Brnaches to be Deleted & Prompt for Deletion

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,10 @@ struct Cli {
     // Show the branches that would be deleted, but do not delete any branches
     #[arg(short, long, default_value_t = false)]
     dry_run: bool,
+
+    // Auto-accept any prompts
+    #[arg(short, long, default_value_t = false)]
+    yes: bool
 }
 
 fn main() -> Result<()> {
@@ -18,20 +22,35 @@ fn main() -> Result<()> {
     let repository = Repository::open(&args.repository)
         .with_context(|| format!("Failed to open repository at {:?}", args.repository))?;
    
-     let orphans = repository.branches(Some(BranchType::Local))
-         .unwrap()
+     let orphans: Vec<Branch<'_>> = repository
+         .branches(Some(BranchType::Local))
+         .with_context(|| format!("Failed to get local branches for {:?}", args.repository))?
          .map(|r| r.unwrap().0)
-         .filter(|b| !has_remote(b));
-     
-     for mut o in orphans {
-        let name = o.name()
+         .filter(|b| !has_remote(b))
+         .collect();
+    
+    println!("Going to delete:");
+    let mut to_delete: Vec<Branch> = Vec::new();
+    for o in orphans {
+        let name: &str = o.name()
             .with_context(|| "Failed to get name for a branch")?
             .ok_or(Error::msg("Branch name is not valid UTF-8"))?;
+        println!(" - {}", name);
+        to_delete.push(o)
+    }
+    println!("Ok? [Yn]");
+    
+    if !args.yes {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input)?;
 
-        println!("deleting {}", name);
-        if !args.dry_run {
-            o.delete()?
-        };
+        if input.trim().eq_ignore_ascii_case("y") || input.eq("\n") {
+            if !args.dry_run {
+                for mut b in to_delete {
+                    b.delete()?
+                }
+            }
+        }
     }
 
     Ok(())
