0e9dffae9f248a270f1a7a034f44fc291a455aa0
Christoffer Gram <christofferkgram@gmail.com>
2023-07-13 21:27:07 +0200

    Use clap and anyhow for CLI and Eror Handling

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,40 @@
 use git2::{Repository, BranchType, Branch};
+use clap::Parser;
+use anyhow::{Context, Result, Error};
 
-const REPOSITORY_PATH: &str = ".";
-const REMOTE: &str = "origin";
-fn main() {
-    let repository = match Repository::open(REPOSITORY_PATH) {
-        Ok(repo) => repo,
-        Err(e) => panic!("failed to open: {}", e),
-    };
-
-    let orphans = repository.branches(Some(BranchType::Local))
-        .unwrap()
-        .map(|r| r.unwrap().0)
-        .filter(|b| !has_remote(b));
+#[derive(Parser)]
+struct Cli {
+    // The repository to remove trim
+    repository: std::path::PathBuf,
     
-    for mut o in orphans {
-        println!("deleting {}", o.name().unwrap().unwrap());
-        let _ = o.delete();
+    // Show the branches that would be deleted, but do not delete any branches
+    #[arg(short, long, default_value_t = false)]
+    dry_run: bool,
+}
+
+fn main() -> Result<()> {
+    let args = Cli::parse();
+
+    let repository = Repository::open(&args.repository)
+        .with_context(|| format!("Failed to open repository at {:?}", args.repository))?;
+   
+     let orphans = repository.branches(Some(BranchType::Local))
+         .unwrap()
+         .map(|r| r.unwrap().0)
+         .filter(|b| !has_remote(b));
+     
+     for mut o in orphans {
+        let name = o.name()
+            .with_context(|| "Failed to get name for a branch")?
+            .ok_or(Error::msg("Branch name is not valid UTF-8"))?;
+
+        println!("deleting {}", name);
+        if !args.dry_run {
+            o.delete()?
+        };
     }
+
+    Ok(())
 }
 
 // Assumes that branch.upstream() only fails when there is no upsteam
